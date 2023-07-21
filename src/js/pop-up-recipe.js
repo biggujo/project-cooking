96569cc559befda327ba668793bc0ebe8e337c73
Rhilim <arhilim@gmail.com>
2023-07-21 16:05:09 -0500

    modal

--- src/js/pop-up-recipe.js
+++ src/js/pop-up-recipe.js
@@ -1,4 +1,17 @@
-alert('hey');
-console.log('Hello');
+
 // https://tasty-treats-backend.p.goit.global/api/recipes/recipeID;
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
