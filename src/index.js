a58e4fc1583109831bee869c89477dc838c2eb32
arhip14 <kruvdyk1503@gmail.com>
2023-07-24 22:00:53 +0300

    initial commit2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // * An example of import
 import { sayHello } from './js/test.js';
+import './js/filter-markup.js'
 
 
 
--- src/js/filter-markup.js
+++ src/js/filter-markup.js
@@ -1,32 +1,77 @@
-// Функція для отримання даних рецепту з бекенду та створення картки рецепту
 async function fetchRecipeData() {
-  try {
+    try {
+      const apiUrl =
+        "https://tasty-treats-backend.p.goit.global/api/recipes?category=Beef&page=1&limit=6&time=160&area=Irish&ingredients=640c2dd963a319ea671e3796";
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const recipeData = data.results[0];
 
-    const apiUrl = "https://tasty-treats-backend.p.goit.global/api/recipes?category=Beef&page=1&limit=6&time=160&area=Irish&ingredients=640c2dd963a319ea671e3796";
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    const recipeData = data.recipes[0]; 
+      const recipeCardTemplate = `
+        <div class="recipe-card">
+                  <svg
+            class="heart-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            class="like-icon"
+          >
+            <use href="#like" />
+          </svg>
+          <img
+            src="${recipeData.image}"
+            alt="Recipe Image"
+            class="recipe-bg-image"
+            style="background-image: url('${recipeData.image}')"
+          />
+          <h2 class="recipe-title">${recipeData.title}</h2>
+          <p class="recipe-description">${recipeData.description}</p>
+          <div class="rating">
+          <div class="rating-number">${recipeData.rating}</div>
+            <div class="rating-stars"></div>
+            
+          </div>
+        </div>
+      `;
 
-    const recipeCardTemplate = `
-      <div class="recipe-card">
-        <h2 class="recipe-title">${recipeData.title}</h2>
-        <p class="recipe-description">${recipeData.description}</p>
-        <div class="rating">Rating: ${recipeData.rating}</div>
-        <button class="see-recipe-btn">See Recipe</button>
-        <svg class="icon-close"></svg>
-      </div>
-    `;
+      const recipeContainer = document.getElementById("recipe-container");
+      recipeContainer.innerHTML = recipeCardTemplate;
+      updateRatingStars(recipeData.rating);
 
-
-    const recipeContainer = document.getElementById("recipe-container");
-    recipeContainer.innerHTML = recipeCardTemplate;
-    const seeRecipeButton = document.querySelector(".see-recipe-btn");
-    seeRecipeButton.addEventListener("click", () => {
-      
-      console.log("Переглянути рецепт");
-    });
-  } catch (error) {
-    console.error("Помилка при отриманні даних з бекенду:", error);
+      const heartIcon = recipeContainer.querySelector(".heart-icon");
+      heartIcon.addEventListener("click", function () {
+        toggleFavourite(recipeData.title);
+      });
+    } catch (error) {
+      console.error("Error fetching data from the backend:", error);
+    }
   }
-}
+
+  function updateRatingStars(rating) {
+    const ratingContainer = document.querySelector(".rating-stars");
+    const filledStars = Math.round(rating);
+    const emptyStars = 5 - filledStars;
+
+    let ratingStarsHTML = "";
+    for (let i = 0; i < filledStars; i++) {
+      ratingStarsHTML += '<span class="rating-star filled">&#9733;</span>';
+    }
+    for (let i = 0; i < emptyStars; i++) {
+      ratingStarsHTML += '<span class="rating-star">&#9733;</span>';
+    }
+
+    ratingContainer.innerHTML = ratingStarsHTML;
+  }
+
+  function toggleFavourite(recipeTitle) {
+    const heartIcon = document.querySelector(".heart-icon");
+    const isFilled = heartIcon.classList.contains("active");
+
+    if (isFilled) {
+      console.log("Remove from favourites: ", recipeTitle);
+    } else {
+      console.log("Add to favourites: ", recipeTitle);
+    }
+
+    heartIcon.classList.toggle("active");
+  }
+
 fetchRecipeData();
