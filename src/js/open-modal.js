70e620138a48da14eed9b8bda341ea8c2ff14aa3
VladyslavSimahin <vladyslav.simahin@gmail.com>
2023-07-24 00:54:23 -0600

    update header

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -12,3 +12,25 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    lightTheme: document.querySelector('[data-light-theme]'),
+    darkTheme: document.querySelector('[data-dark-theme]'),
+    switch: document.querySelector('[data-button]'),
+  };
+
+  refs.lightTheme.addEventListener('click', () => {
+    if (!refs.lightTheme.classList.contains('is-hiddenss')) {
+      refs.lightTheme.classList.add('is-hiddenss');
+      refs.darkTheme.classList.remove('is-hiddenss');
+    }
+  });
+
+  refs.darkTheme.addEventListener('click', () => {
+    if (!refs.darkTheme.classList.contains('is-hiddenss')) {
+      refs.darkTheme.classList.add('is-hiddenss');
+      refs.lightTheme.classList.remove('is-hiddenss');
+    }
+  });
+})();
--- src/js/ordernow.js
+++ src/js/ordernow.js
@@ -12,3 +12,19 @@
     refs.modal.classList.toggle('is-hiddens');
   }
 })();
+
+const form = document.getElementById('order-form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  console.log('Name:', name);
+  console.log('Phone number:', phone);
+  console.log('Email:', email);
+  console.log('Comment:', message);
+});
