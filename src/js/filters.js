748505d16cf6570cbc311834761f31ae83950ed4
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-07-24 20:22:40 +0100

    Change requests in filters.js
    added axios, then/catch;
    alphabetical sorting of selects;
    added reusable functions

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {CustomSelect} from './custom-select';
 
 const closeIconEl = document.querySelector('.search__close-icon');
@@ -34,40 +35,61 @@ function clearFiltersInput (){
     }
 }
 
-(async() => {
-    const response = await fetch('https://tasty-treats-backend.p.goit.global/api/areas')
-    if (response.ok) {
-      const data = await response.json();
-      const values = Object.keys(data).map((key, index) => {
-        return `<li class="filter-select__option" data-select="option" data-value="${key}" data-index="${index}">${data[key].name}</li>`;
-      });
-      document.querySelector('#options-area').innerHTML = values.join('');
-      new CustomSelect('#select-area');
-      document.querySelector('#toggle-area').disabled = false;
+/* Fetch and rendering dropdown */
+
+function fetchDataForFilters(filterName) {
+  const API_URL = 'https://tasty-treats-backend.p.goit.global/api';
+  return axios
+  .get(`${API_URL}/${filterName}`)
+  .then(response => {
+    if (response.status === 200) {
+      const data = response.data;
+      data.sort((x, y) => x.name.localeCompare(y.name));
+      return data;
     }
-})();
-(async() => {
-    const response = await fetch('https://tasty-treats-backend.p.goit.global/api/ingredients')
-    if (response.ok) {
-      const data = await response.json();
-      const values = Object.keys(data).map((key, index) => {
-        return `<li class="filter-select__option" data-select="option" data-value="${key}" data-index="${index}">${data[key].name}</li>`;
-      });
-      document.querySelector('#options-ingredients').innerHTML = values.join('');
-      new CustomSelect('#select-ingredients');
-      document.querySelector('#toggle-ingredients').disabled = false;
-    }
-})();
-function renderTime () {
-    let arr = []; 
-    for (let i = 5; i <= 120; i+= 5) {
-      arr.push(i);
-    }
-    const values = arr.map((key, index) => {
-        return `<li class="filter-select__option" data-select="option" data-value="${key}" data-index="${index}">${key} min</li>`;
-    });
-    document.querySelector('#options-time').innerHTML = values.join('');
-    new CustomSelect('#select-time');
-    document.querySelector('#toggle-time').disabled = false;
+  })
+  .catch(error => {
+    console.error('ERROR', error);
+  });
 }
-renderTime ();
+
+function renderFilterList (filterName, values) {
+  document.querySelector(`#options-${filterName}`).innerHTML = values.join('');
+  new CustomSelect(`#select-${filterName}`);
+  document.querySelector(`#toggle-${filterName}`).disabled = false;
+  
+}
+
+function renderTime () {
+  let arr = []; 
+  for (let i = 5; i <= 120; i+= 5) {
+    arr.push(i);
+  }
+  const values = arr.map((key, index) => {
+    return `<li class="filter-select__option" data-select="option" data-value="${key}" data-index="${index}">${key} min</li>`;
+  });
+  renderFilterList('time', values);
+};
+renderTime();
+
+fetchDataForFilters('ingredients')
+.then(data => {
+  const values = Object.keys(data).map((key, index) => {
+    return `<li class="filter-select__option" data-select="option" data-value="${data[key]._id}" data-index="${index}">${data[key].name}</li>`;
+  });
+  renderFilterList('ingredients', values);
+})
+.catch(error => {
+  console.error('ERROR', error);
+});
+
+fetchDataForFilters('areas')
+.then(data => {
+  const values = Object.keys(data).map((key, index) => {
+    return `<li class="filter-select__option" data-select="option" data-value="${data[key].name}" data-index="${index}">${data[key].name}</li>`;
+  });
+  renderFilterList('area', values);
+})
+.catch(error => {
+  console.error('ERROR', error);
+});
