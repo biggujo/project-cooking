7b1f290f1d59ea4fbeb23448d0834a174e2cf8bf
biggujo <gen.style22@gmail.com>
2023-07-24 18:12:23 +0300

    Add pop-up modal class

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 import { sayHello } from './js/test.js';
 
 import './js/all-categories.js';
+import { PopUpModal } from './js/pop-up-modal.js';
+
+// Recipe modal
+new PopUpModal({
+  openModalSelector: '[data-pop-up-rating-open]',
+  closeModalSelector: '[data-pop-up-rating-close]',
+  backdropSelector: '[data-pop-up-rating-modal]',
+});
+
+// Rating modal
+new PopUpModal({
+  openModalSelector: '[data-pop-up-rating-open]',
+  closeModalSelector: '[data-pop-up-rating-close]',
+  backdropSelector: '[data-pop-up-rating-modal]',
+});
