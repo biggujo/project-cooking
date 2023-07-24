1a1c65e01545e15563757e1694f6dca633eef711
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-07-24 14:13:49 +0300

    Add styles to images of slider and text of dish and country to the second image of the slides

--- src/js/createHeroSliderMarkup.js
+++ src/js/createHeroSliderMarkup.js
@@ -1,12 +1,26 @@
 export const createHeroSliderMarkup = async data => {
   console.log(await data);
   return (await data)
-    .map(({ cook, topic }) => {
+    .map(({ cook, topic }, index) => {
       return `
-        <div class="swiper-slide hero-swiper-slide">
-          <div class="hero-swiper-slide-wrapper hero-cook-slide-wrapper"><img class="hero-swiper-img" src="${cook.imgWebpUrl}" alt="${cook.name}"></div>
-          <div class="hero-swiper-slide-wrapper hero-info-slide-wrapper"><img class="hero-swiper-info-img" src="${topic.previewWebpUrl}" alt="${topic.name}"></div>
-          <div class="hero-swiper-slide-wrapper hero-img-slide-wrapper"><img class="hero-swiper-img" src="${topic.imgWebpUrl}" alt="${topic.name}"></div>
+        <div class="swiper-slide hero-swiper-slide hero-swiper-slide-${
+          index + 1
+        }" style="display: flex">
+          <div class="hero-swiper-slide-wrapper hero-cook-slide-wrapper"><img class="hero-swiper-img" src="${
+            cook.imgWebpUrl
+          }" alt="${cook.name}"></div>
+          <div class="hero-swiper-slide-wrapper hero-info-slide-wrapper">
+            <img class="hero-swiper-info-img" src="${
+              topic.previewWebpUrl
+            }" alt="${topic.name}">
+            <div class="hero-swiper-info-descr">
+              <p class="hero-swiper-info-dish">${topic.name}</p>
+              <span class="hero-swiper-info-country">${topic.area}</span>
+            </div>
+          </div>
+          <div class="hero-swiper-slide-wrapper hero-img-slide-wrapper"><img class="hero-swiper-img hero-swiper-dish-img" src="${
+            topic.imgWebpUrl
+          }" alt="${topic.name}"></div>
         </div>
    `;
     })
