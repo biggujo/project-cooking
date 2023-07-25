fb3cd150b77d0c4e105f4f7b117ca80a39deb8c2
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-07-25 19:59:45 +0300

    add Function to change the theme and Styles for dark theme

---
+++ src/js/switcher.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+  const body = document.querySelector('body');
+  body.classList.toggle('dark-theme'); 
+  
+  const isDarkTheme = body.classList.contains('dark-theme');
+  localStorage.setItem('darkTheme', isDarkTheme);
+}
+
+const themeToggleBtn = document.querySelector('.switch-button');
+themeToggleBtn.addEventListener('click', toggleTheme);
+
+const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+if (isDarkTheme) {
+  const body = document.querySelector('body');
+  body.classList.add('dark-theme');
+}
