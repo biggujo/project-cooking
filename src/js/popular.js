5a392e8b9c29918c69469094697a11de9ed874a1
MAKCKACHKA <doubleakop@gmail.com>
2023-07-25 18:38:23 +0300

    Finished cutting words and some edits

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const listDiv = document.querySelector('.popular-recipes-list');
+const popularRecipesItems = document.querySelectorAll('.popular-recipes-item');
 const loader = document.querySelector('.loader');
 
 // function showLoader() {
@@ -11,27 +12,15 @@ function hideLoader() {
   loader.classList.add('is-hidden');
 }
 
-function truncateText(text, limit) {
-  if (text.length <= limit) {
-    return text;
-  } else {
-    return text.slice(0, limit) + '...';
-  }
+function throttle(f, t) {
+  return function (args) {
+    let previousCall = this.lastCall;
+    this.lastCall = Date.now();
+    if (previousCall === undefined || this.lastCall - previousCall > t) {
+      f(args);
+    }
+  };
 }
-
-function getTruncateLength() {
-  const screenWidth = window.screen.availWidth;
-
-  if (screenWidth < 768) {
-    return 80;
-  } else if (screenWidth >= 768 && screenWidth < 1280) {
-    return 64;
-  } else if (screenWidth >= 1280) {
-    return 85;
-  }
-}
-
-// const getEventData = async () => {
 
 function getEventData() {
   return axios
@@ -40,69 +29,37 @@ function getEventData() {
 }
 
 function renderPopular(item) {
-  const truncatedDescription = truncateText(
-    item.description,
-    getTruncateLength()
-  );
   return `
     <li class="popular-recipes-item" attribute-id="${item._id}">
       <img class="popular-recipes-image" src="${item.preview}" alt="${item.title}" />
       <div class="popular-recipes-text">
         <h3 class="popular-recipes-text-title">${item.title}</h3>
-        <p class="popular-recipes-text-description">${truncatedDescription}</p>
+        <p class="popular-recipes-text-description">${item.description}</p>
       </div>
     </li>
   `;
 }
+
+function renderPopularItems(data) {
+  listDiv.innerHTML = '';
+  const maxIterations = window.screen.availWidth < 768 ? 2 : data.length;
+  for (let i = 0; i < maxIterations; i++) {
+    const item = data[i];
+    const masterClassHTML = renderPopular(item);
+    listDiv.innerHTML += masterClassHTML;
+    hideLoader();
+  }
+}
+
 export function popular() {
-  function renderPopularItems(data) {
-    listDiv.innerHTML = '';
-    const maxIterations = window.screen.availWidth < 768 ? 2 : data.length;
-    for (let i = 0; i < maxIterations; i++) {
-      const item = data[i];
-      const masterClassHTML = renderPopular(item);
-      listDiv.innerHTML += masterClassHTML;
-      hideLoader();
-    }
-
-    const popularRecipesItems = document.querySelectorAll(
-      '.popular-recipes-item'
-    );
-    // popularRecipesItems.forEach(item => {
-    //   item.addEventListener('click', () => {
-    //     const attributeId = item.getAttribute('attribute-id');
-    //     function getId() {
-    //       return fetch(
-    //         `https://tasty-treats-backend.p.goit.global/api/recipes/${attributeId}`
-    //       )
-    //         .then(response => response.json())
-    //         .then(data => {
-    //           console.log(data);
-    //         })
-    //         .catch(error => {
-    //           console.error(error);
-    //         });
-    //     }
-    //     getId();
-    //   });
-    // });
+  function resize() {
+    getEventData()
+      .then(data => {
+        renderPopularItems(data);
+      })
+      .catch(error => console.error(error));
   }
 
-  let resizeTimer;
-  function handleResize() {
-    if (resizeTimer) {
-      clearTimeout(resizeTimer);
-    }
-
-    resizeTimer = setTimeout(() => {
-      getEventData()
-        .then(data => {
-          renderPopularItems(data);
-        })
-        .catch(error => console.error(error));
-    }, 100);
-  }
-
-  window.addEventListener('resize', handleResize);
-  handleResize();
+  window.addEventListener('resize', throttle(resize, 100));
+  resize();
 }
