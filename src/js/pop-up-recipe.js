986bbd7343e8df88a392793f4e4c3542e7e0a693
Rhilim <arhilim@gmail.com>
2023-07-25 11:37:16 -0500

    Add to Favorites BTN local Storage add

--- src/js/pop-up-recipe.js
+++ src/js/pop-up-recipe.js
@@ -36,14 +36,42 @@ function onEscapeKeyPress(e) {
 
 // get Data for Modal Window
 
+
 async function getRecipeInfo() {
   try {
     const apiUrl =
       'https://tasty-treats-backend.p.goit.global/api/recipes?category=Beef&page=1&limit=6&time=160&area=Irish&ingredients=640c2dd963a319ea671e3796';
     const response = await fetch(apiUrl);
     const data = await response.json();
+    // console.log(data);
     const recipeData = data.results[0];
+    // console.log(data.results[0]);
+   
+    
+    // colorPickerOptions.find(option => option.label === "blue")
+    const res = await fetch(
+      'https://tasty-treats-backend.p.goit.global/api/ingredients'
+    );
+    const arrOfIngredients = await res.json();
+    // let idName = [];
+    // const {id, measure} = recipeData.ingredients;
+    //   const{_id, name} = arrOfIngredients;
+    //   // console.dir(name);
+    // // console.log(arrOfIngredients);
+    // // const showId = arrOfIngredients.map(ingredient => ingredient.name);
+    // // console.log(showId);
+    // arrOfIngredients.forEach((element) => {
+    //   // console.log(element.name);
+    //   // console.log(id);
+    //   // console.log(element._id);
+    //   if(id === element._id){
+    //     idName = element.name;
+        
+    //     return idName;
+    //   }
+    // });
 
+      console.log(recipeData.ingredients);
     const ingredientsMarkup = recipeData.ingredients
       .map(({ id, measure }) => {
         return `<li class='ingred-li-item'>
@@ -52,26 +80,6 @@ async function getRecipeInfo() {
               </li>`;
       })
       .join('');
-
-
-
-    // if (ingredientsMarkup.id === showId._id) {
-    //   const idName = showId.name;
-    //   console.log(idName);
-    // }
-
-
-
-    const res = await fetch(
-      'https://tasty-treats-backend.p.goit.global/api/ingredients'
-    );
-    const arrOfIngredients = await res.json();
-    console.log(arrOfIngredients);
-    const showId = arrOfIngredients.map(ingredient => ingredient.name);
-    console.log(showId);
-
-
-
 
 
     const tagsMarkup = recipeData.tags
@@ -84,49 +92,49 @@ async function getRecipeInfo() {
 
     const modalWindowRecipeMarkup = `
     <div class='switch-places'>
-            <iframe
-          class='video-link'
-          src='https://www.youtube.com/embed/${getYouTubeVideoID(
-            recipeData.youtube
-          )}'
-          title='YouTube video player'
-          frameborder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowfullscreen
-        ></iframe>
-        <h1 class='pop-up-recipe-title'>${recipeData.title}</h1>
-      </div>
-        <div class='media-container'>
-          <div class='rating'>
-            <span class='middle-rate'>${recipeData.rating}</span>
-            <ul class='stars-list'>
-              <li><svg class='star-svg'>
-                <use href='./img/icons.svg#star'></use>
-              </svg></li>
-              <li><svg class='star-svg'>
-                <use href='./img/icons.svg#star'></use>
-              </svg></li>
-              <li><svg class='star-svg'>
-                <use href='./img/icons.svg#star'></use>
-              </svg></li>
-              <li><svg class='star-svg'>
-                <use href='./img/icons.svg#star'></use>
-              </svg></li>
-              <li><svg class='star-svg'>
-                <use href='./img/icons.svg#star'></use>
-              </svg></li>
-            </ul>
-            <span class='middle-time'>${recipeData.time}</span>
-          </div>
-          <div class='ingredients'>
-            <ul class='ingred-list'>
-              ${ingredientsMarkup}
-            </ul>
-          </div>
-             <ul class='hashtag-list'>
-              ${tagsMarkup}
-            </ul>
+          <iframe
+        class='video-link'
+        src='https://www.youtube.com/embed/${getYouTubeVideoID(
+          recipeData.youtube
+        )}'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowfullscreen
+      ></iframe>
+      <h1 class='pop-up-recipe-title'>${recipeData.title}</h1>
+    </div>
+      <div class='media-container'>
+        <div class='rating'>
+          <span class='middle-rate'>${recipeData.rating}</span>
+          <ul class='stars-list'>
+            <li><svg class='star-svg'>
+              <use href='./img/icons.svg#star'></use>
+            </svg></li>
+            <li><svg class='star-svg'>
+              <use href='./img/icons.svg#star'></use>
+            </svg></li>
+            <li><svg class='star-svg'>
+              <use href='./img/icons.svg#star'></use>
+            </svg></li>
+            <li><svg class='star-svg'>
+              <use href='./img/icons.svg#star'></use>
+            </svg></li>
+            <li><svg class='star-svg'>
+              <use href='./img/icons.svg#star'></use>
+            </svg></li>
+          </ul>
+          <span class='middle-time'>${recipeData.time}</span>
         </div>
+        <div class='ingredients'>
+          <ul class='ingred-list'>
+            ${ingredientsMarkup}
+          </ul>
+        </div>
+          <ul class='hashtag-list'>
+            ${tagsMarkup}
+          </ul>
+      </div>
             <p class='how-to-cook'>
                 ${recipeData.instructions}
               </p>
@@ -134,6 +142,30 @@ async function getRecipeInfo() {
 
     const modalWindow = document.querySelector('.modal-recipe-content');
     modalWindow.innerHTML = modalWindowRecipeMarkup;
+
+// Add to FAVORITES
+
+const favBtn  = document.querySelector('.fav');
+favBtn.addEventListener('click', onFavBtnClick);
+let localStorageData = [];
+
+function onFavBtnClick(evt) {
+  // evt.preventDefault();
+
+  const localStorCheck = localStorage.getItem('favorites');
+  const parsed = JSON.parse(localStorCheck);
+  console.log(parsed);
+
+  if(!parsed){
+    localStorageData.push(`${recipeData.title}`); 
+    localStorage.setItem('favorites', JSON.stringify(localStorageData));
+    favBtn.textContent = 'Remove from Favorites';
+  } else if (localStorageData.includes(`${recipeData.title}`)){
+    favBtn.textContent = 'Remove from Favorites';
+    localStorage.setItem
+    return
+  }
+}
   } catch (error) {
     console.log(error);
   }
@@ -144,64 +176,4 @@ function getYouTubeVideoID(url) {
   const match = url.match(regExp);
 
   return match && match[2].length === 11 ? match[2] : null;
-}
-
-
-
-// Add to FAVORITES
-
-const favBtn  = document.querySelector('.fav');
-favBtn.addEventListener('click', onFavBtnClick);
-const localStorageData = [];
-
-function onFavBtnClick(evt) {
-  evt.preventDefault();
-  console.log(evt.target);
-
-  localStorageData.push(id); 
- 
-  localStorage.setItem('favorites', JSON.stringify(localStorageData));
-}
-
-
-
-
-
-function toggleFavourite(recipeTitle) {
-  const heartIcon = document.querySelector(".like-icon");
-  const isFilled = heartIcon.classList.contains("filled");
-
-  if (!isFilled) {
-    console.log("Add to favourites: ", recipeTitle);
-    addToFavorites(recipeTitle);
-  } else {
-    console.log("Remove from favourites: ", recipeTitle);
-    removeFromFavorites(recipeTitle);
-  }
-}
-
-function addToFavorites(recipeTitle) {
-  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  if (!favorites.includes(recipeTitle)) {
-    favorites.push(recipeTitle);
-    saveToFavorites(favorites);
-  }
-}
-
-function removeFromFavorites(recipeTitle) {
-  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  const index = favorites.indexOf(recipeTitle);
-  if (index !== -1) {
-    favorites.splice(index, 1);
-    saveToFavorites(favorites);
-  }
-}
-
-function saveToFavorites(favorites) {
-  localStorage.setItem("favorites", JSON.stringify(favorites));
-}
-
-function toggleHeartFill() {
-  const heartIcon = document.querySelector(".like-icon");
-  heartIcon.classList.toggle("filled");
 }
