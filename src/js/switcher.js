1011ef9379e32b0afd9f1a910103a11e5de8cc7b
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-07-26 21:53:35 +0300

    Comments have been removed

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,36 +1,3 @@
-// function toggleTheme() {
-//   const body = document.querySelector('body');
-//   body.classList.toggle('dark-theme');
-
-//   const isDarkTheme = !body.classList.contains('dark-theme');
-//   localStorage.setItem('darkTheme', isDarkTheme);
-// }
-
-// const themeToggleBtn = document.querySelector('.switch-input');
-// themeToggleBtn.addEventListener('click', toggleTheme);
-
-// const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
-// if (isDarkTheme) {
-//   const body = document.querySelector('body');
-//   body.classList.add('dark-theme');
-// }
-
-// function toggleTheme() {
-//   const body = document.querySelector('body');
-//   body.classList.toggle('dark-theme');
-
-//   const isDarkTheme = !body.classList.contains('dark-theme');
-//   localStorage.setItem('darkTheme', isDarkTheme);
-// }
-
-// const themeToggleBtn = document.querySelector('.switch-input');
-// themeToggleBtn.addEventListener('click', toggleTheme);
-
-// const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
-// if (isDarkTheme) {
-//   const body = document.querySelector('body');
-//   body.classList.add('dark-theme');
-// }
 const body = document.querySelector('body');
 const themeToggleBtns = document.querySelectorAll('.switch-input');
 
