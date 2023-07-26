0e53d12fb55a0b233ef82b7943145c6c94a9ca30
biggujo <gen.style22@gmail.com>
2023-07-26 12:30:35 +0300

    Add RecipeCard class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // * An example of import
 import { sayHello } from './js/test.js';
-import './js/filter-markup.js'
 
 import './js/all-categories.js';
 import { PopUpModal } from './js/pop-up-modal.js';
+import { RecipeCard } from './js/recipe-card.js';
 
 // Recipe modal
 new PopUpModal({
@@ -18,3 +18,9 @@ new PopUpModal({
   closeModalSelector: '[data-pop-up-recipe-close]',
   backdropSelector: '[data-pop-up-recipe-modal]',
 });
+
+const card = new RecipeCard()
+  .init('6462a8f74c3d0ddd28897fb8')
+  .then(recipeCardEl => {
+    document.body.prepend(recipeCardEl);
+  });
