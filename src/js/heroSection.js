9f2a9463429ee23cd38f11dfac028cac88f0fa91
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-07-26 15:34:31 +0300

    Fix change theme btn and styles

---
+++ src/js/heroSection.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { fetchEvents } from './fetchEvents';
+import { createHeroSliderMarkup } from './createHeroSliderMarkup.js';
+
+const eventsSwiperRef = document.querySelector('.swiper-wrapper');
+
+fetchEvents().then(async resp => {
+  const eventsMarkup = await createHeroSliderMarkup(resp);
+
+  eventsSwiperRef.innerHTML = eventsMarkup;
+
+  const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    modules: [Pagination],
+    spaceBetween: 8,
+    breakpoints: {
+      768: {
+        spaceBetween: 16,
+      },
+    },
+    pagination: {
+      clickable: true,
+      el: '.swiper-pagination',
+    },
+  });
+});
--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,16 +1,51 @@
+// function toggleTheme() {
+//   const body = document.querySelector('body');
+//   body.classList.toggle('dark-theme');
+
+//   const isDarkTheme = !body.classList.contains('dark-theme');
+//   localStorage.setItem('darkTheme', isDarkTheme);
+// }
+
+// const themeToggleBtn = document.querySelector('.switch-input');
+// themeToggleBtn.addEventListener('click', toggleTheme);
+
+// const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+// if (isDarkTheme) {
+//   const body = document.querySelector('body');
+//   body.classList.add('dark-theme');
+// }
+
+// function toggleTheme() {
+//   const body = document.querySelector('body');
+//   body.classList.toggle('dark-theme');
+
+//   const isDarkTheme = !body.classList.contains('dark-theme');
+//   localStorage.setItem('darkTheme', isDarkTheme);
+// }
+
+// const themeToggleBtn = document.querySelector('.switch-input');
+// themeToggleBtn.addEventListener('click', toggleTheme);
+
+// const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+// if (isDarkTheme) {
+//   const body = document.querySelector('body');
+//   body.classList.add('dark-theme');
+// }
+const body = document.querySelector('body');
+const themeToggleBtn = document.querySelector('.switch-input');
+
 function toggleTheme() {
-  const body = document.querySelector('body');
   body.classList.toggle('dark-theme');
 
-  const isDarkTheme = !body.classList.contains('dark-theme');
-  localStorage.setItem('darkTheme', isDarkTheme);
+  const isDarkTheme = JSON.parse(localStorage.getItem('darkTheme'));
+  localStorage.setItem('darkTheme', !isDarkTheme);
 }
 
-const themeToggleBtn = document.querySelector('.switch-input');
-themeToggleBtn.addEventListener('click', toggleTheme);
+const isDarkTheme = JSON.parse(localStorage.getItem('darkTheme'));
 
-const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
 if (isDarkTheme) {
-  const body = document.querySelector('body');
   body.classList.add('dark-theme');
+  themeToggleBtn.checked = true;
 }
+
+themeToggleBtn.addEventListener('click', toggleTheme);
