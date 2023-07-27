68e20a6f05d12a778b7436a1b3d3519bf0da8793
biggujo <gen.style22@gmail.com>
2023-07-27 19:28:32 +0300

    Add card and categories render

--- src/favorites.js
+++ src/favorites.js
@@ -3,47 +3,83 @@ import { RecipeCard } from './js/recipe-card.js';
 console.log('Hello!');
 
 const refs = {
+  favoritesCategories: document.querySelector('.fav-categories'),
   favoritesList: document.getElementById('rendered-cards-for-favourites'),
 };
 
-renderCardsByIds();
+const categories = new Set();
 
 document.addEventListener('remove-from-favorites', () => {
   console.log('Change!');
-  renderCardsByIds();
+  doNewRenderOfCardsAndCategories();
 });
 
-function renderCardsByIds() {
+doNewRenderOfCardsAndCategories();
+
+function doNewRenderOfCardsAndCategories() {
+  renderCardsByIds()
+    .then(categories => {
+      console.log(categories);
+      renderCategoriesByNames(categories);
+    })
+    .catch(console.log);
+}
+
+async function renderCardsByIds() {
   try {
     const idsArray = JSON.parse(localStorage.getItem('favorites'));
 
     refs.favoritesList.innerHTML = '';
 
-    idsArray.forEach(id => {
-      new RecipeCard().init(id).then(recipeCardEl => {
-        const itemEl = document.createElement('li');
-        itemEl.classList.add('fav-categories-item');
-        itemEl.appendChild(recipeCardEl.recipeCardEl);
-
-        refs.favoritesList.appendChild(itemEl);
-      });
+    const cardsPromises = idsArray.map(id => {
+      return new RecipeCard().init(id);
     });
+
+    const recipeCardEls = await Promise.all(cardsPromises);
+
+    const categories = recipeCardEls.map(recipeCardEl => {
+      const itemEl = document.createElement('li');
+      itemEl.classList.add('fav-categories-item');
+
+      itemEl.appendChild(recipeCardEl.recipeCardEl);
+
+      refs.favoritesList.appendChild(itemEl);
+
+      return recipeCardEl.recipeData.category;
+    });
+
+    return categories;
   } catch (error) {
     console.log(error);
   }
 }
 
+function renderCategoriesByNames(categories) {
+  const ALL_CATEGORIES_NAME = 'All categories';
+
+  refs.favoritesCategories.innerHTML = '';
+
+  renderCategoryByName(ALL_CATEGORIES_NAME);
+  categories.forEach(renderCategoryByName);
+
+  function renderCategoryByName(categoryName = '') {
+    refs.favoritesCategories.insertAdjacentHTML(
+      'beforeend',
+      `<button data-category='${
+        categoryName === ALL_CATEGORIES_NAME ? '' : categoryName
+      }'>${categoryName}</button>`
+    );
+  }
+}
+
 new RecipeCard().init('6462a8f74c3d0ddd28897fb8').then(recipeCardEl => {
   document.body.prepend(recipeCardEl.recipeCardEl);
-  console.log(recipeCardEl);
 });
 
 new RecipeCard().init('6462a8f74c3d0ddd28897fb9').then(recipeCardEl => {
   document.body.prepend(recipeCardEl.recipeCardEl);
-  console.log(recipeCardEl);
 });
 
 new RecipeCard().init('6462a8f74c3d0ddd28897fba').then(recipeCardEl => {
   document.body.prepend(recipeCardEl.recipeCardEl);
-  console.log(recipeCardEl);
 });
