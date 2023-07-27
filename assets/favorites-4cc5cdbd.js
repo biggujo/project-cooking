93165002e67ce5a5fee858d062885358cb610fc1
biggujo <37770603+biggujo@users.noreply.github.com>
2023-07-27 11:57:19 +0000

    Deploying to gh-pages from @ biggujo/project-cooking@1a84ebc5a770c13f2730afb0cc36cdeb5d138e7d 🚀

--- assets/favorites-4cc5cdbd.js
+++
@@ -1,1 +0,0 @@
-import{h as r,s as h}from"./scroll-up-b9603435.js";import"./icons-e10878ea.js";r();h();
---
+++ assets/favorites-bf8de6f8.js
@@ -0,0 +1,1 @@
+import{h as r,s as h}from"./scroll-up-b9603435.js";import"./icons-ca31f8c1.js";r();h();
